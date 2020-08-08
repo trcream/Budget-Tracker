8c869d9a2ef7b3b48c770fe6f589442da698554e
Tcreamer <trentoncreamer@yahoo.com>
2020-08-08 08:42:44 -0700

    mongoose connection editd

--- Develop/server.js
+++ Develop/server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
